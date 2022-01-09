5cab13078d649c80beae80e965fcd573f9f9da84
Rishi Srivastava <72429483+sri-rishi@users.noreply.github.com>
2022-01-09 21:19:07 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var readlinesync = require("readline-sync");
 var chalk = require("chalk");
 
 var userName = readlinesync.question(chalk.blue("Enter your name - "));
-console.log(chalk.cyan("Welcome " + userName));
+console.log(chalk.cyan(`Welcome ${userName}`));
 console.log(chalk.cyan("Lets check your number is palindrome or not"));
 var userString = readlinesync.question("Enter your number - ")
 var reverseString = userString.split("").reverse().join("");
 
 if (reverseString === userString ){
-  console.log(chalk.yellowBright(userString + " is palindrome string"));
+  console.log(chalk.yellowBright( `${userString} is palindrome string`));
   console.log(chalk.green(reverseString));
 }else{
-  console.log(chalk.red(userString + " is not palindrome string"));
+  console.log(chalk.red( `${userString} is not palindrome string`));
 }
